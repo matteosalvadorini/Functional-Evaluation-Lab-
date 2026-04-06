e915f1e48193cff91d485eb829b7d22ca2bc1da7
Matteo <Matteo@DESKTOP-RRIE64D.homenet.telecomitalia.it>
2026-04-06 15:18:16 +0200

    Optimizing the plotting with loop for raw and filtered data

--- EMG.m
+++ EMG.m
@@ -1,4 +1,4 @@
-%% 1 - Plot RAW DATA
+%% INTRO
 
 % Right Leg (1-9)
 % 1. Tibialis anterior R 
@@ -18,123 +18,90 @@
 % 15. Vastus Med L 
 % 16. Semitendinous L 
 
+channels = {'Tibialis Ant R', 'Gastro Lat R', 'Soleus R', 'Gastro Med R', ...
+           'Rectus R', 'Vastus Lat R', 'Vastus Med R', 'Semitendinous R', ...
+           'Tibialis Ant L', 'Gastro Lat L', 'Soleus L', 'Gastro Med L', ...
+           'Rectus L', 'Vastus Lat L', 'Vastus Med L', 'Semitendinous L', 'Trigger'};
 
-n_sample = size(leftGAL_resampled, 1);
+
+
+
+
+%% PLOT RAW DATA
+
+%convert struct to array: easier to plot
+data_array = struct2array(data_resampled);
+
+%calculate time for one channel, will be the same length for the others
+n_sample = size(data_array, 1);
 t = (0:n_sample-1) / fs_channels;
 t=t';
 
-figure()
-tiledlayout(6,3)
+page=6;
 
-nexttile;
-plot(t,rightTA_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Tibialis anterior R ');
+for i = 1:17
 
-nexttile;
-plot(t,rightGAL_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Gastro lat R');
 
-nexttile;
-plot(t,rightSOL_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('SoleusR');
+    % --- PAGE BREAK LOGIC ---
+    % Creates a new figure every 6 channels
+    if mod(i-1, page) == 0
+        figure('Units', 'normalized', 'Position', [0.1 0.1 0.8 0.8]);
+        t = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Grid 3x2 per page
+        on_page = ceil(i / page);
+        title(t, ['EMG raw data - Page ', num2str(on_page)]);
+    end
 
-nexttile;
-plot(t,rightGAM_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Gastro med R');
 
-nexttile;
-plot(t,rightRF_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Rectus R');
+    nexttile;
+    plot(data_array(:,i));
+    title(channels{i}, 'Interpreter', 'none');
+end
 
-nexttile;
-plot(t,rightVL_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Vastus Let R');
-
-nexttile;
-plot(t,rightVM_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Vastus Med R');
-
-nexttile;
-plot(t,rightSM_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Semitendinous R');
-
-nexttile;
-plot(t,leftTA_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Tibialis anterior L');
-
-nexttile;
-plot(t,leftGAL_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Gastro lat L');
-
-nexttile;
-plot(t,leftSOL_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Soleus L');
-
-nexttile;
-plot(t,leftGAM_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Gastro med L');
-
-nexttile;
-plot(t,leftRF_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Rectus L');
-
-nexttile;
-plot(t,leftVL_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Vastus Lat L');
-
-nexttile;
-plot(t,leftGAM_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Vastus Med L');
-
-nexttile;
-plot(t,leftSM_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('Semitedinous L');
-
-nexttile;
-plot(t,trigger_resampled), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
-title('trigger');
 
 %% BAND-PASS FILTERING
 
 % Band-pass filter 5th order
-% Matlab functions: Butter(); filtfilt()
 % cut off frequency = [20-400] Hz 
-
-
 W= [20; 400];
-Wn = W/(fs_channels/2); %frequenza noralizzata
+Wn = W/(fs_channels/2); %normalized frequency
 [b,a ]= butter(5,Wn,"bandpass");
 
-leftGALisnan = fillmissing(leftGAL_resampled, 'constant', 0);
-
-leftGAL_f = filtfilt(b,a,leftGALisnan);
 
 
-n_sample = size(leftGAL_f, 1);
+%cancel NaN values: every NaN values = 0 with:
+data_isnan=fillmissing(data_array, 'constant', 0);
+%filter for every channel
+data_f= filtfilt(b,a,data_isnan);
+
+
+
+%calculate AGAIN (gives problem with dimension) time for one channel, will be the same length for the others
+n_sample = size(data_f, 1);
 t_f = (0:n_sample-1) / fs_channels;
 t_f=t_f';
 
 
-% plot of the filtered data 
+page=6;
 
-figure()
+for i = 1:17
 
-plot(t_f,leftGAL_f), xlabel('time [s]'), ylabel('Crank angle [°]'), xlim([0 max(t)]),sgtitle('Right Leg','FontSize',14,'FontWeight','b');
-set(gca,'FontSize',12);
+ 
+    % --- PAGE BREAK LOGIC ---
+    % Creates a new figure every 6 channels
+
+    if mod(i-1, page) == 0
+        figure('Units', 'normalized', 'Position', [0.1 0.1 0.8 0.8]);
+        t = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Griglia 3x2 per pagina
+        on_page = ceil(i / page);
+        title(t, ['EMG_filtered - Page ', num2str(on_page)]);
+    end
+
+
+    nexttile;
+    plot(data_f(:,i));
+    title(channels{i}, 'Interpreter', 'none');
+end
+
+
+
+%%
