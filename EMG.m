e5cbb08794ec658cbfb1a312d2bcb60494422835
Matteo <Matteo@10.175.248.165>
2026-04-09 17:39:27 +0200

    fixed crank angle

--- EMG.m
+++ EMG.m
@@ -120,37 +120,36 @@ end
 
 
 fs = 2222.22;
-trigger_chan = data{:, 17}; 
+trigger_chan = data_array(:, 17); 
 
-% 1. Trova gli indici dei picchi (i tuoi "segnalibri")
-[~, stops] = findpeaks(trigger_chan, 'MinPeakHeight', 2, 'MinPeakDistance', fs*1);
+% 1. Trova i picchi. 
+% Se il segnale è rumoroso, prova ad abbassare MinPeakHeight a 1 o 1.5
+[~, stops] = findpeaks(trigger_chan, 'MinPeakHeight', 0.2, 'MinPeakDistance', fs*1);
 
-% 2. Crea il tempo totale (CrankAngle_time) - NON RIPARTE DA ZERO
-CrankAngle_time = (0:length(trigger_chan)-1)' / fs;
+% --- CONTROLLO DI SICUREZZA ---
+if isempty(stops)
+    error('ATTENZIONE: Nessun picco trovato nel canale trigger! Controlla la soglia MinPeakHeight.');
+end
 
-% 3. Crea l'Angolo Continuo (CrankAngle)
-CrankAngle = zeros(length(trigger_chan), 1); 
+% 2. Inizializza il vettore CrankAngle
+CrankAngle = zeros(size(trigger_chan)); 
 
+% 3. Ciclo per riempire ogni rivoluzione tra 0 e 360
 for i = 1:length(stops)-1
     start_idx = stops(i);
     end_idx = stops(i+1);
     
-    % Calcoliamo l'accumulo dei gradi: 
-    % Giro 1: da 0 a 360
-    % Giro 2: da 360 a 720
-    % Giro 3: da 720 a 1080...
-    offset_gradi = (i-1) * 360;
-    
     n_points = end_idx - start_idx;
-    
-    % Genera i gradi per questo specifico intervallo
-    CrankAngle(start_idx:end_idx-1) = linspace(offset_gradi, offset_gradi + 360, n_points);
+    CrankAngle(start_idx:end_idx-1) = linspace(0, 360, n_points);
 end
 
-% Riagganciamo l'ultima parte dopo l'ultimo picco (opzionale)
-CrankAngle(stops(end):end) = (offset_gradi + 360);
+% 4. Gestione dei bordi (solo se stops non è vuoto)
+if stops(1) > 1
+    CrankAngle(1:stops(1)-1) = NaN;
+end
+CrankAngle(stops(end):end) = NaN;
 
-
+CrankAngle_time = (0:length(trigger_chan)-1)' / fs;
 %% IDENTIFICATION OF VOLUNTARY PEDALING PHASE 
 
 [ii,ang]=ginput(2);
@@ -163,20 +162,47 @@ CrankAngle(stops(end):end) = (offset_gradi + 360);
 
 figure()
 subplot(10,1,1), plot(CrankAngle_time,CrankAngle),ylabel('Crank angle'), xlim([min(CrankAngle_time) max(CrankAngle_time)]),title('Right Leg');
-subplot(10,1,2), plot(t,data_array(:,1)), ylabel('EMG-GlMax'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,3), plot(t,data_array(:,2)), ylabel('EMG-BFlong'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,4), plot(t,data_array(:,3)), ylabel('EMG-BFshort'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,5), plot(t,data_array(:,4)), ylabel('EMG-GL'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,6), plot(t,data_array(:,5)), ylabel('EMG-So'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,7), plot(t,data_array(:,6)), ylabel('EMG-TFL'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,8), plot(t,data_array(:,7)), ylabel('EMG-RF'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,9), plot(t,data_array(:,8)), ylabel('EMG-VL'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,10), plot(t,data_array(:,9)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,11), plot(t,data_array(:,10)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,12), plot(t,data_array(:,11)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,13), plot(t,data_array(:,12)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,14), plot(t,data_array(:,13)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,15), plot(t,data_array(:,14)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,16), plot(t,data_array(:,15)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
-subplot(10,1,17), plot(t,data_array(:,16)), ylabel('EMG-TA'), xlim([min(EMG_time) max(EMG_time)]);
+subplot(10,1,2), plot(t,data_array(:,1)), ylabel('EMG-GlMax'), xlim([min(t) max(t)]);
+subplot(10,1,3), plot(t,data_array(:,2)), ylabel('EMG-BFlong'), xlim([min(t) max(t)]);
+subplot(10,1,4), plot(t,data_array(:,3)), ylabel('EMG-BFshort'), xlim([min(t) max(t)]);
+subplot(10,1,5), plot(t,data_array(:,4)), ylabel('EMG-GL'), xlim([min(t) max(t)]);
+subplot(10,1,6), plot(t,data_array(:,5)), ylabel('EMG-So'), xlim([min(t) max(t)]);
+subplot(10,1,7), plot(t,data_array(:,6)), ylabel('EMG-TFL'), xlim([min(t) max(t)]);
+subplot(10,1,8), plot(t,data_array(:,7)), ylabel('EMG-RF'), xlim([min(t) max(t)]);
+subplot(10,1,9), plot(t,data_array(:,8)), ylabel('EMG-VL'), xlim([min(t) max(t)]);
+subplot(10,1,10), plot(t,data_array(:,9)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,11), plot(t,data_array(:,10)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,12), plot(t,data_array(:,11)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,13), plot(t,data_array(:,12)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,14), plot(t,data_array(:,13)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,15), plot(t,data_array(:,14)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,16), plot(t,data_array(:,15)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+subplot(10,1,17), plot(t,data_array(:,16)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
+
+
+
+%% 4 - Power spectral density estimate & extraction of spectral parameters
+
+
+[P_EMG,F] = periodogram(data_f, rectwin(max(size(data_f))),512,fs); 
+
+% plot of the power spectral density estimate of the right RF
+figure ()
+subplot(2,1,1)
+plot(F,10*log10(P_EMG(:,7)),'b')
+title('Periodogram Power Spectral Density Estimate','b')
+xlabel('Frequency (Hz)')
+ylabel('Power/frequency (dB/Hz)')
+grid on
+ylim([ -120 0])
+xlim([0 fs/2])
+
+%estimates the mean frequency & median frequency
+for n=1:12
+       Mean_freq(n,1)=meanfreq( data_f(:,n) , fs );
+       Mean_freq(n,2) = meanfreq( P_EMG(:,n) , F ); 
+
+       Med_freq(n,1) = medfreq( data_f(:,n) , fs );
+       Med_freq(n,2) = medfreq( P_EMG(:,n) , F ); 
+end
 
