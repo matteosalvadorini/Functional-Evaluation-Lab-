f98d4a6d963beec10a0c01ca0e7d12d8479e009c
Matteo <Matteo@DESKTOP-RRIE64D>
2026-04-16 16:28:15 +0200

    time normalization

--- EMG.m
+++ EMG.m
@@ -46,9 +46,9 @@ for i = 1:17
     % Creates a new figure every 6 channels
     if mod(i-1, page) == 0
         figure('Units', 'normalized', 'Position', [0.1 0.1 0.8 0.8]);
-        t = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Grid 3x2 per page
+        ti = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Grid 3x2 per page
         on_page = ceil(i / page);
-        title(t, ['EMG raw data - Page ', num2str(on_page)]);
+        title(ti, ['EMG raw data - Page ', num2str(on_page)]);
     end
 
 
@@ -91,9 +91,9 @@ for i = 1:17
 
     if mod(i-1, page) == 0
         figure('Units', 'normalized', 'Position', [0.1 0.1 0.8 0.8]);
-        t = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Griglia 3x2 per pagina
+        ti = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Griglia 3x2 per pagina
         on_page = ceil(i / page);
-        title(t, ['EMG_filtered - Page ', num2str(on_page)]);
+        title(ti, ['EMG_filtered - Page ', num2str(on_page)]);
     end
 
 
@@ -122,7 +122,7 @@ end
 fs = 2222.22;
 trigger_chan = data_array(:, 17); 
 
-% 1. Trova i picchi. 
+% 1.  findpeaks
 % Se il segnale è rumoroso, prova ad abbassare MinPeakHeight a 1 o 1.5
 [~, stops] = findpeaks(trigger_chan, 'MinPeakHeight', 0.2, 'MinPeakDistance', fs*1);
 
@@ -150,41 +150,11 @@ end
 CrankAngle(stops(end):end) = NaN;
 
 CrankAngle_time = (0:length(trigger_chan)-1)' / fs;
-%% IDENTIFICATION OF VOLUNTARY PEDALING PHASE 
-
-[ii,ang]=ginput(2);
-
-[m,locsxi]=min(abs(CrankAngle_time-ii(1)));
-[m,locsei]=min(abs(t-ii(1)));
-[m,locsxf]=min(abs(CrankAngle_time-ii(2)));
-[m,locsef]=min(abs(t-ii(2)));
-
-
-figure()
-subplot(10,1,1), plot(CrankAngle_time,CrankAngle),ylabel('Crank angle'), xlim([min(CrankAngle_time) max(CrankAngle_time)]),title('Right Leg');
-subplot(10,1,2), plot(t,data_array(:,1)), ylabel('EMG-GlMax'), xlim([min(t) max(t)]);
-subplot(10,1,3), plot(t,data_array(:,2)), ylabel('EMG-BFlong'), xlim([min(t) max(t)]);
-subplot(10,1,4), plot(t,data_array(:,3)), ylabel('EMG-BFshort'), xlim([min(t) max(t)]);
-subplot(10,1,5), plot(t,data_array(:,4)), ylabel('EMG-GL'), xlim([min(t) max(t)]);
-subplot(10,1,6), plot(t,data_array(:,5)), ylabel('EMG-So'), xlim([min(t) max(t)]);
-subplot(10,1,7), plot(t,data_array(:,6)), ylabel('EMG-TFL'), xlim([min(t) max(t)]);
-subplot(10,1,8), plot(t,data_array(:,7)), ylabel('EMG-RF'), xlim([min(t) max(t)]);
-subplot(10,1,9), plot(t,data_array(:,8)), ylabel('EMG-VL'), xlim([min(t) max(t)]);
-subplot(10,1,10), plot(t,data_array(:,9)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,11), plot(t,data_array(:,10)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,12), plot(t,data_array(:,11)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,13), plot(t,data_array(:,12)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,14), plot(t,data_array(:,13)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,15), plot(t,data_array(:,14)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,16), plot(t,data_array(:,15)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-subplot(10,1,17), plot(t,data_array(:,16)), ylabel('EMG-TA'), xlim([min(t) max(t)]);
-
-
 
 %% 4 - Power spectral density estimate & extraction of spectral parameters
 
 
-[P_EMG,F] = periodogram(data_f, rectwin(max(size(data_f))),512,fs); 
+[P_EMG,F] = periodogram(data_array, rectwin(max(size(data_array))),512,fs); 
 
 % plot of the power spectral density estimate of the right RF
 figure ()
@@ -199,10 +169,152 @@ xlim([0 fs/2])
 
 %estimates the mean frequency & median frequency
 for n=1:12
-       Mean_freq(n,1)=meanfreq( data_f(:,n) , fs );
+       Mean_freq(n,1)=meanfreq( data_array(:,n) , fs );
        Mean_freq(n,2) = meanfreq( P_EMG(:,n) , F ); 
 
-       Med_freq(n,1) = medfreq( data_f(:,n) , fs );
+       Med_freq(n,1) = medfreq( data_array(:,n) , fs );
        Med_freq(n,2) = medfreq( P_EMG(:,n) , F ); 
 end
 
+
+%% 6 - Identification of the pedaling cycles
+% Find the end of each pedaling cycling based on the CrankAngle
+[pks_angle,locs_angle] = findpeaks(trigger_chan, 'MinPeakHeight', 0.2, 'MinPeakDistance', fs*1);
+
+
+figure()
+ax1=subplot(211);
+plot(CrankAngle_time,CrankAngle), ylabel('Crank Angle'), xlabel('Time [s]'),title('Peaks Angle');
+hold on, plot(CrankAngle_time(locs_angle),CrankAngle(locs_angle),'o')
+
+% Locate the same peaks on the EMG data
+
+locs_emg=zeros(size(locs_angle));
+
+for j=1:length(locs_angle)
+    [m,locs_emg(j)]=min(abs(t-CrankAngle_time(locs_angle(j))));
+end
+
+ax2=subplot(212);
+plot(t,data_f(:,7)), ylabel('EMG-RF'), xlabel('Time [s]'),title('Cycles identification on EMG signals');
+hold on, plot(t(locs_emg),data_f(locs_emg,7),'o')
+
+linkaxes([ax1, ax2],'x')
+
+
+%% 7 - Time normalization of all pedaling cycles
+
+AngBase=linspace(0,359,360);
+
+for n=1:17 %number of EMG channels
+    EMG_mat{n}.values=zeros(360,size(locs_emg,1)-1);
+    EMG_matOK{n}.values=zeros(360,30);
+    EMG_matOK_norm{n}.values=zeros(360,30);
+end
+
+
+for i = 1: size(locs_emg)-1
+    
+    t_orig = linspace(locs_emg(i)+1,locs_emg(i+1),locs_emg(i+1)-locs_emg(i));
+    t_N = linspace(locs_emg(i)+1,locs_emg(i+1),360);
+    
+    for n=1:12
+        EMG_mat{n}.values(:,i) = interp1(t_orig,data_f(locs_emg(i)+1:locs_emg(i+1),n), t_N, 'spline');
+    end
+    
+end
+
+c = [0 0.4470 0.7410];
+
+
+
+
+page=6;
+
+for i = 1:17
+
+
+    % --- PAGE BREAK LOGIC ---
+    % Creates a new figure every 6 channels
+    if mod(i-1, page) == 0
+        figure('Units', 'normalized', 'Position', [0.1 0.1 0.8 0.8]);
+        ti = tiledlayout(3, 2, 'TileSpacing', 'compact'); % Grid 3x2 per page
+        on_page = ceil(i / page);
+        title(ti, ['EMG raw data - Page ', num2str(on_page)]);
+    end
+
+
+    nexttile;
+    plot(AngBase,EMG_mat{i}.values,'Color',c), ylabel('EMG - GlMax'), xlim([0  360]), xlabel('Crank angle [°]');
+
+    title(channels{i}, 'Interpreter', 'none');
+end
+
+%% 8 - Identification of the first 30 pedaling cycles at target cadence +/- 4RPM
+
+time_CYCLE=CrankAngle_time(locs_angle);
+mean_cadence=60./diff(time_CYCLE); %mean cadence in RPM
+
+target_cadence=35;
+good_cycle=find(and(mean_cadence<=target_cadence+4, mean_cadence>=target_cadence-4),30);
+
+
+figure
+plot(time_CYCLE(1:end-1),mean_cadence,'--*'), xlabel ('#cycles'),ylabel('cadence [rpm]')
+hold on
+plot(time_CYCLE(good_cycle),mean_cadence(good_cycle),'--r*'), xlabel ('#cycles'),ylabel('cadence [rpm]')
+
+
+%% 9 - Amplitude normalization to median peak & 10 - Calculation of mean activation profile 
+
+EMG_mean=zeros(9,360);
+
+figure(30)
+for n=1:17
+    EMG_matOK{n}.values(:,1:30)=data_f{n}.values(:,good_cycle);
+    
+    norm_value(n)=median(max(EMG_matOK{n}.values));
+    EMG_matOK_norm{n}.values(:,:)=EMG_matOK{n}.values(:,:)./norm_value(n);
+    
+    EMG_mean(n,:)= mean(EMG_matOK_norm{n}.values');
+    EMG_std(n,:)= std(EMG_matOK_norm{n}.values');
+    
+    
+    if (n<10)
+        subplot(5,2,n)
+        hold on
+        plot(AngBase,EMG_matOK{n}.values,'r')
+    end
+end
+
+figure
+sgtitle('Right leg','FontSize',14, 'FontWeight','b')
+for n=1:9
+    subplot(5,2,n);
+    plot(AngBase,EMG_matOK_norm{n}.values,'Color',c),  xlim([0  360]), xlabel('Crank angle [°]');
+    hold on
+    plot(AngBase,EMG_mean(n,:),'Color','k','LineWidth',2)
+    plot(AngBase,EMG_mean(n,:)-EMG_std(n,:),'--k','LineWidth',2)
+    plot(AngBase,EMG_mean(n,:)+EMG_std(n,:),'--k','LineWidth',2)
+    
+    switch n
+        case 1
+            ylabel('EMG norm - GlMax')
+        case 2
+            ylabel('EMG norm - BFlong')
+        case 3
+            ylabel('EMG norm - BFshort')
+        case 4
+            ylabel('EMG norm - GL')
+        case 5
+            ylabel('EMG norm - So')
+        case 6
+            ylabel('EMG norm - TFL')
+        case 7
+            ylabel('EMG norm - RF')
+        case 8
+            ylabel('EMG - VL')
+        case 9
+            ylabel('EMG - TA')
+    end
+end
